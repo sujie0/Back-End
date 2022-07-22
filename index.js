26ef1a2336cd62e0f0c530f62ea2016fe1c47922
ponyoinumc <heoeunjae0825@gmail.com>
2022-07-22 17:57:36 +0900

    userId 검색 라우트 추가

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 //IP 주소가 바뀌면 안드로이드 앱 내에 있는 url 주소도 바꿔주어야 정상 동작함
 
-var express = require('express');
-var http = require('http');
-var bodyParser= require('body-parser');
+var express = require("express");
+var http = require("http");
+var bodyParser = require("body-parser");
 var app = express();
 var port_num = 3000;
 
-const login = require('./routes/login');
-const signup = require('./routes/signup');
-const userId = require('./routes/userId');
+const login = require("./routes/login");
+const signup = require("./routes/signup");
+const userId = require("./routes/searchUserId");
 
-app.get('', (req, res, next) => {
-    res.send("메인 페이지")
+app.get("", (req, res, next) => {
+  res.send("메인 페이지");
 });
 
-app.use('/login', login);
-app.use('/signup', signup);
-app.use('/userId',userId);
+app.use("/login", login);
+app.use("/signup", signup);
+n;
+app.use("/userId", userId);
 
-app.set('port',process.env.PORT || port_num);
-app.use(bodyParser.urlencoded({extended:false}));
+app.set("port", process.env.PORT || port_num);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
+  console.log("일정 제목, 일정 설명, 가능한 날들 받아오기");
+  var approve = { "일정 제목": "NO", "일정 설명": "NO", "선택된 날짜": "NO" };
 
-    console.log('일정 제목, 일정 설명, 가능한 날들 받아오기');
-    var approve ={'일정 제목':'NO','일정 설명':'NO','선택된 날짜': 'NO'};
+  var paramName = req.body.name;
+  var paramExplain = req.body.explain;
+  var paramYear = req.body.year;
+  var paramMonth = req.body.month;
+  var paramDay = req.body.day;
 
-    var paramName = req.body.name;
-    var paramExplain = req.body.explain;
-    var paramYear=req.body.year;
-    var paramMonth=req.body.month;
-    var paramDay=req.body.day;
+  if (!paramYear)
+    return res.send(response(baseResponse.DATESELECTION_YEAR_EMPTY));
 
-    if(!paramYear)
-        return res.send(response(baseResponse.DATESELECTION_YEAR_EMPTY));
+  if (paramYear.length != 4)
+    return res.send(response(baseResponse.DATESELECTION_YEAR_LENGTH));
 
-    if(paramYear.length!=4)
-        return res.send(response(baseResponse.DATESELECTION_YEAR_LENGTH));
-    
+  if (!paramMonth)
+    return res.send(response(baseResponse.DATESELECTION_MONTH_EMPTY));
 
-    if(!paramMonth)
-        return res.send(response(baseResponse.DATESELECTION_MONTH_EMPTY));
+  if (paramMonth.length != 2)
+    return res.send(response(baseResponse.DATESELECTION_MONTH_LENGTH));
 
-    if(paramMonth.length!=2)
-        return res.send(response(baseResponse.DATESELECTION_MONTH_LENGTH));
-    
+  if (!paramDay)
+    return res.send(response(baseResponse.DATESELECTION_DAY_EMPTY));
 
-    if(!paramDay)
-        return res.send(response(baseResponse.DATESELECTION_DAY_EMPTY));
+  if (paramDay.length != 2)
+    return res.send(response(baseResponse.DATESELECTION_DAY_LENGTH));
 
-    if(paramDay.length!=2)
-        return res.send(response(baseResponse.DATESELECTION_DAY_LENGTH)); 
+  console.log(
+    "일정 제목 : " +
+      paramName +
+      "  일정 설명 : " +
+      paramExplain +
+      " 선택된 날짜 : " +
+      paramYear +
+      "년 " +
+      paramMonth +
+      "월 " +
+      paramDay +
+      "일"
+  );
 
-
-    console.log('일정 제목 : '+paramName+'  일정 설명 : '+paramExplain+' 선택된 날짜 : '+paramYear+'년 '+
-    paramMonth+'월 '+paramDay+'일');
-   
-
-    res.send(approve);
-
+  res.send(approve);
 });
 
 /*
@@ -68,4 +74,6 @@ var server = http.createServer(app).listen(app.get('port'),function(){
 });
 */
 
-app.listen(3000, () => {console.log(app.get('port')+"port server 실행됨");});
+app.listen(3000, () => {
+  console.log(app.get("port") + "port server 실행됨");
+});
--- routes/userId.js
+++
@@ -1,9 +0,0 @@
-//user id를 통해 특정 유저를 찾는 페이지 구현
-const express = require('express');
-const router = express.Router();
-
-router.get('', (req, res, next) => {
-    res.send("라우팅을 이용하여 user id 검색 페이지에 접속");
-});
-
-module.exports = router;
