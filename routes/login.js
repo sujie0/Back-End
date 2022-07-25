a8406eb4e9fc383376c864f7998c303c9d1a06f4
sujie0 <sujie0@naver.com>
2022-07-25 18:30:33 +0900

    회원가입, 로그인 기능 구현

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,110 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res, next) => {
-    res.send("라우팅을 이용하여 login 페이지에 접속");
+    var title = '로그인';
+    var description = "ID와 PW를 입력해주세요.";
+    var html = `
+    <!doctype html> 
+    <html> 
+    <head><title>${title}</title> <meta charset="utf-8"></head> 
+    <body>
+        <h2>로그인</h2>
+        <form action = "/login/done" method = "post">
+        <ul>
+            <p> ID
+            <input type = "text" name = "id" placeholder = "id">
+            </p>
+        </ul>
+        <ul>
+            <p> PW
+            <input type = "text" name = "pw" placeholder = "pw">
+            </p>
+        </ul>
+        <p>
+            <input type="submit" value = "로그인">
+        </p>
+        </form>
+
+    </body> 
+    </html>
+    `;
+
+    res.writeHead(200);
+    res.end(html);
+
+    //res.send("라우팅을 이용하여 login 페이지에 접속");
 });
 
+// 로그인 성공적으로 완료되었는지 판단
 router.get('/done', (req, res, next) => {
-    res.send("로그인이 완료되었습니다!");
+    var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        
+        request.on('end', function(req){
+            var post = qs.parse(body);
+            const obj = JSON.parse(JSON.stringify(post));
+            var keys = Object.keys(obj);
+
+            var mysql = require('mysql');
+           
+            // DB 연결
+            var con = mysql.createConnection({
+                host: '', 
+                port: '',
+                user: '', 
+                password: '', 
+                database: ''
+            });
+
+            // 데이터 베이스가 연결되었는지 확인
+            con.connect((err) => {
+                if (err) {
+                    console.log(err);
+                    con.end();
+                    throw err;
+                } else {
+                    console.log("DB 접속 성공");
+                }
+            });
+
+            module.exports = con;
+
+            var sql = 'SELECT user_id, user_pw FROM tb_user WHERE user_id = ?';
+            var params = [obj[keys[0]]];
+            con.query(sql, params, function(err, result){
+                try {
+                    var input_id = obj[keys[0]];
+                    var input_pw = obj[keys[1]];
+
+                    var valid_id = result[0].user_id;
+                    var valid_pw = result[0].user_pw;
+
+                    if(input_id === valid_id){
+                        if(input_pw === valid_pw){
+                            console.log('로그인이 완료되었습니다.');
+                            con.end();
+                            response.writeHead(302, {Location : '/'});
+                            response.end();
+                            
+                        } else {
+                            console.log('비밀번호가 틀렸습니다.');
+                            con.end();
+                            response.writeHead(302, {Location : '/routes/login'});
+                            response.end();
+
+                        };
+                    }                    
+                } catch (error) {
+                    console.log('존재하지 않는 회원입니다');
+                    con.end();
+                    response.writeHead(302, {Location : '/routes/signup'});
+                    response.end();
+                }
+            });
+            
+        });
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -11,7 +11,7 @@ router.get('', (req, res, next) => {
     <head><title>${title}</title> <meta charset="utf-8"></head> 
     <body>
         <h2>회원 가입</h2>
-        <form action = "/done" method = "post">
+        <form action = "/signup/done" method = "post">
         <ul>
             <p> 이름  
             <input type = "text" name = "name" placeholder = "name">
@@ -51,9 +51,65 @@ router.get('', (req, res, next) => {
     res.send("라우팅을 이용하여 signup 페이지에 접속");
 });
 
+/*이미 존재하는 id를 회원가입할 때 입력하는 경우에 대한 예외처리 필요*/
+
+//회원가입을 성공적으로 완료한 경우
 router.get('/done', (req, res, next) => {
+// 이 부분에서 데이터를 db로 넘기기
 
     res.send("회원가입이 완료되었습니다!");
+    var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(req){
+            var post = qs.parse(body);
+            const obj = JSON.parse(JSON.stringify(post)); 
+            var keys = Object.keys(obj);
+            
+            var mysql = require('mysql');
+
+            // 데이터 베이스 정보 불러오기
+            var con = mysql.createConnection({
+                host: 'localhost', 
+                port: '3306',
+                user: 'root', 
+                password: 'oe4989',
+                database: 'timetuning'
+            });
+    
+            // 데이터 베이스가 연결되었는지 확인
+            con.connect((err) => {
+                if (err) {
+                    console.log(err);
+                    con.end();
+                    throw err;
+                } else {
+                    console.log("DB 접속 성공");
+                }
+            });
+
+            module.exports = con;
+
+            // sql문
+            var sql = 'INSERT INTO new_table(user_name, user_id, user_pw, user_email) VALUES(?,?,?,?)';
+            var params = [obj[keys[0]],obj[keys[1]],obj[keys[2]],obj[keys[3]]]
+            con.query(sql, params, function(err, rows, fields){
+                if(err){
+                    console.log(err);
+                } else{
+                    console.log(rows.name);
+                }
+            });
+
+            con.end();
+
+
+            // 전송 후 첫화면으로 돌아가기
+            response.writeHead(302, {Location : '/'});
+            response.end();
+        });
+
 });
 
 module.exports = router;
