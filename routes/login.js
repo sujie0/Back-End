2ccb1b1513ae7461d7d422f73c08ffbfd2b7c5e9
sujie0 <sujie0@naver.com>
2022-07-28 14:29:23 +0900

    로그인, 회원가입 -> 예외처리 구현

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 //로그인 페이지 구현
+const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -33,80 +34,5 @@ router.get('', (req, res, next) => {
 
     res.writeHead(200);
     res.end(html);
-
-    //res.send("라우팅을 이용하여 login 페이지에 접속");
 });
-
-// 로그인 성공적으로 완료되었는지 판단
-router.get('/done', (req, res, next) => {
-    var body = '';
-        request.on('data', function(data){
-            body = body + data;
-        });
-        
-        request.on('end', function(req){
-            var post = qs.parse(body);
-            const obj = JSON.parse(JSON.stringify(post));
-            var keys = Object.keys(obj);
-
-            var mysql = require('mysql');
-           
-            // DB 연결
-            var con = mysql.createConnection({
-                host: 'timetuning.cwrxmkgsy5uc.ap-northeast-2.rds.amazonaws.com', 
-                port: '3306',
-                user: 'admin', 
-                password: '12345678', 
-                database: 'timetuning'
-            });
-
-            // 데이터 베이스가 연결되었는지 확인
-            con.connect((err) => {
-                if (err) {
-                    console.log(err);
-                    con.end();
-                    throw err;
-                } else {
-                    console.log("DB 접속 성공");
-                }
-            });
-
-            module.exports = con;
-
-            var sql = 'SELECT user_id, user_pw FROM tb_user WHERE user_id = ?';
-            var params = [obj[keys[0]]];
-            con.query(sql, params, function(err, result){
-                try {
-                    var input_id = obj[keys[0]];
-                    var input_pw = obj[keys[1]];
-
-                    var valid_id = result[0].user_id;
-                    var valid_pw = result[0].user_pw;
-
-                    if(input_id === valid_id){
-                        if(input_pw === valid_pw){
-                            console.log('로그인이 완료되었습니다.');
-                            con.end();
-                            response.writeHead(302, {Location : '/'});
-                            response.end();
-                            
-                        } else {
-                            console.log('비밀번호가 틀렸습니다.');
-                            con.end();
-                            response.writeHead(302, {Location : '/routes/login'});
-                            response.end();
-
-                        };
-                    }                    
-                } catch (error) {
-                    console.log('존재하지 않는 회원입니다');
-                    con.end();
-                    response.writeHead(302, {Location : '/routes/signup'});
-                    response.end();
-                }
-            });
-            
-        });
-});
-
 module.exports = router;
