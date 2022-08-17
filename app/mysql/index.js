ed650892dc96d12d62ebcf214e032228e4941a62
ponyoinumc <heoeunjae0825@gmail.com>
2022-08-17 18:35:25 +0900

    schedule 연결 데이터베이스  수정

--- app/mysql/index.js
+++ app/mysql/index.js
@@ -4,11 +4,11 @@ const sql = require("./sql.js");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '127.0.0.1',
-  port: 3307,
-  user: 'root',
-  password: '1234',
-  database: 'test'
+  host: 'timetuning.cwrxmkgsy5uc.ap-northeast-2.rds.amazonaws.com',
+  port: 3306,
+  user: 'admin',
+  password: '12345678',
+  database: 'timetuning'
 });
 
 const query = async (alias, values) => {
